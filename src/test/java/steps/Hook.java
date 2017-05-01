ec620f4fdd132f90ee6358bb8a545f953b2930e3
LifeAtHorizon <alfredjoey1@gmail.com>
2017-05-01 14:56:22 -0400

    Update Hook.java

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -29,7 +29,7 @@ public class Hook extends BaseUtil{
 
 
         //Chrome driver
-        System.setProperty("webdriver.chrome.driver", "https://github.com/LifeAtHorizon/SeleniumWithCucucumber.git");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\venka\\Desktop\\External jars\\chromedriver.exe");
         base.Driver = new ChromeDriver();
     }
 
